6d674a01bec368d2c1656bda22f4e13ab4f78331
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-04-12 17:22:26 -0400

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// if hiring radio button is selected
+function revealRate() {
+  document.getElementById("hrate").style.display = "block";
+}
+// if others is selected
+function hideRate() {
+  document.getElementById("hrate").style.display = "none";
+}
